fcd109309bb32cd10cb69295c9500535e130d75c
Ali <ali.mekanik7@gmail.com>
2022-07-15 13:34:53 +0430

    output type decloration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidStyled from "babel-plugin-solid-styled";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
@@ -9,15 +11,22 @@ export default defineConfig({
         plugins: [[solidStyled, { prefix: "sg" }]],
       },
     }),
+    dts({
+      insertTypesEntry: true,
+      noEmitOnError: true,
+      skipDiagnostics: false,
+      logDiagnostics: true,
+    }),
   ],
   build: {
     lib: {
-      entry: "./src/index.ts",
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
       fileName: "solid-graph",
       name: "SolidGraph",
     },
     rollupOptions: {
-      external: ["solidjs"],
+      external: ["solid-js", "solid-js/web", "solid-js/store"],
       output: {
         dir: "./dist",
       },
